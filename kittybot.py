0d3c20f1b925e906d4cd443ddb082212117d3e10
Vladislav Malygin <malyginvlad6220@gmail.com>
2024-09-25 20:01:56 +0300

    gif added

--- kittybot.py
+++ kittybot.py
@@ -1,6 +1,7 @@
 import requests
 import os
 import logging
+import time
 
 from telebot import TeleBot, types
 from dotenv import load_dotenv
@@ -51,21 +52,6 @@ def get_new_kitten_image():
         print(f'Ошибка: {response.status_code}')
         return None
 
-def get_new_cat_gif():
-    try:
-        response = requests.get(URL)
-        response.raise_for_status()
-    except Exception as error:
-        logging.error(f'Ошибка при запросе к основному API: {error}')
-        new_url = 'https://api.thedogapi.com/v1/images/search'
-        response = requests.get(new_url)
-        response.raise_for_status()
-        print("Извините, мы уже чиним картинки с котиками, вот вам пока картинка с собачкой.")
-
-    response = response.json()
-    random_cat = response[0].get('url')
-    return random_cat
-
 
 # Добавляем хендлер для команды /newcat:
 @bot.message_handler(commands=['newcat'])
@@ -86,6 +72,18 @@ def send_kitten(message):
         bot.send_message(chat.id, "Извините, не удалось получить изображение котенка.")
 
 
+@bot.message_handler(commands=['gif'])
+def send_new_kitten_gif(message):
+    url = f'https://cataas.com/cat/gif?position=center&rand={time.time()}'
+    response = requests.get(url, headers={'accept': 'image/*'})
+
+    chat = message.chat
+
+    if response.status_code == 200:
+        bot.send_animation(chat.id, response.url)  # Отправляем гифку
+    else:
+        bot.send_message(chat.id, "Извините, не удалось получить изображение котенка.")
+
 
 
 @bot.message_handler(commands=['start'])
@@ -97,8 +95,9 @@ def wake_up(message):
     # Создаём объект кнопки:
     button_newcat = types.KeyboardButton('/newcat')
     button_kitten = types.KeyboardButton('/kitten')
+    button_gif = types.KeyboardButton('/gif')
     # Добавляем объекты кнопок на клавиатуру:
-    keyboard.add(button_newcat, button_kitten)
+    keyboard.add(button_newcat, button_kitten,button_gif)
 
     bot.send_message(
         chat_id=chat.id,
